4991021c41fdd2d2cd5389b9747999e8300166b6
Kaoukinn <haujin.kuo@gmail.com>
2023-03-21 15:49:57 +0800

    march21.2

--- convertTemperature2469.py
+++ convertTemperature2469.py
@@ -1,15 +1,8 @@
-celsius = 36.50
-
-anslist = []
-Fahrenheit = round(celsius * 1.80 + 32.00, 5)
-Kelvin = round(celsius + 273.15, 5)
-anslist.append(Kelvin)
-anslist.append(Fahrenheit)
-print(anslist)
-# Kelvin = ("{:.5f}".format(Kelvin))
-# Fahrenheit = ("{:.5f}".format(Fahrenheit))
-
-# anslist.append("{:.5f}".format(Kelvin))
-# anslist.append("{:.5f}".format(Fahrenheit))
-# new_list = [float(x) for x in anslist]
-# print(new_list)
+class Solution:
+    def convertTemperature(self, celsius: float) -> List[float]:
+        anslist = []
+        Fahrenheit = celsius * 1.80 + 32.00
+        Kelvin = celsius + 273.15
+        anslist.append(Kelvin)
+        anslist.append(Fahrenheit)
+        return anslist
